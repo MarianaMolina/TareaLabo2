62cf00f869fa2f717724ce58a43a2d4e10f56d83
Mariana Molina <37163483+MarianaMolina@users.noreply.github.com>
2020-04-06 17:09:49 -0600

    tarea laboratorio 2

---
+++ tareaLabo2/src/main/java/com/uca/capas/domain/Usuario.java
@@ -0,0 +1,23 @@
+package com.uca.capas.domain;
+
+public class Usuario {
+	private String nombre;
+	private String contraseña;
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String Nombre) {
+		this.nombre = Nombre;
+	}
+	
+	public String getContraseña() {
+		return contraseña;
+	}
+	
+	public void setContraseña(String Contraseña) {
+		this.contraseña = Contraseña;
+	}
+
+}
