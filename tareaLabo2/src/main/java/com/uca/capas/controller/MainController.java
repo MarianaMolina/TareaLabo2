195cd8e20553c29c1b09eea967f3c85bf50b42de
Mariana Molina <37163483+MarianaMolina@users.noreply.github.com>
2020-04-06 20:43:21 -0600

    tarea de labo 2

--- tareaLabo2/src/main/java/com/uca/capas/controller/MainController.java
+++ tareaLabo2/src/main/java/com/uca/capas/controller/MainController.java
@@ -1,6 +1,10 @@
 package com.uca.capas.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+
 import com.uca.capas.domain.Usuario;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,15 +14,16 @@ public class MainController {
 	
 	@GetMapping("/login")
 	public String login(Usuario usuario) {
-		return "LOGIN";
+		return "login";
 	}
 	
 	@PostMapping("/validacion")
-	public boolean validacion (Usuario usuario) {
-		boolean flag = false;
-		if (usuario.getNombre() == "Mariana" || usuario.getContra()=="Molina") {
-			return flag = true;
+	public String val(@Valid Usuario usuario) {
+
+		if (usuario.Validacion(usuario.getNombre(), usuario.getContra())== false) {
+			return "MostrarMensajeF";
 		}
-		else return flag;
+
+		return "redirect:/MostrarMensajeV";
 	}
 }
